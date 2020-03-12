1726c21e718683e62424efd9e6b7044debc11583
Ian Dorsey <ian.dorsey@gmail.com>
2020-03-12 15:43:22 -0700

    Created empty /test folder

--- geodata/__init__.py
+++ geodata/__init__.py
@@ -1,29 +1,32 @@
 from Database import Database
+import sys
 
-d = Database()
+print(sys.argv)
 
-print()
-search_name = input("Enter the name of a place that you want to compare with others: ")
-filter_county = input("Would you like to filter by a county? If not, just press Enter: ")
-print()
+# d = Database()
 
-# Obtain the PlaceVector for which we entered a name.
-comparison_pv = \
-    list(filter(lambda x: x.name == search_name, d.placevectors))[0]
+# print()
+# search_name = input("Enter the name of a place that you want to compare with others: ")
+# filter_county = input("Would you like to filter by a county? If not, just press Enter: ")
+# print()
 
-print("The most demographically similar places are:")
-print()
+# # Obtain the PlaceVector for which we entered a name.
+# comparison_pv = \
+#     list(filter(lambda x: x.name == search_name, d.placevectors))[0]
 
-# Filter by county if a filter_county was specified.
-if filter_county != '':
-    filtered_pvs = list(filter(lambda x: x.county == filter_county,
-                        d.placevectors))
+# print("The most demographically similar places are:")
+# print()
 
-# Get the closest PlaceVectors.
-# In other words, get the most demographically similar places.
-closest_pvs = sorted(filtered_pvs,
-    key=lambda x: comparison_pv.distance(x))[1:10]
+# # Filter by county if a filter_county was specified.
+# if filter_county != '':
+#     filtered_pvs = list(filter(lambda x: x.county == filter_county,
+#                         d.placevectors))
 
-# Print these PlaceVectors
-for closest_pv in closest_pvs:
-    print(closest_pv)
+# # Get the closest PlaceVectors.
+# # In other words, get the most demographically similar places.
+# closest_pvs = sorted(filtered_pvs,
+#     key=lambda x: comparison_pv.distance(x))[1:10]
+
+# # Print these PlaceVectors
+# for closest_pv in closest_pvs:
+#     print(closest_pv)
