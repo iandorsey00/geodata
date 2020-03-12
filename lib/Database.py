8b8acb914b110dde04d1fa2f823cac49c156ef88
Ian Dorsey <ian.dorsey@gmail.com>
2020-03-12 14:45:20 -0700

    Created geodata script

--- lib/Database.py
+++ lib/Database.py
@@ -438,28 +438,30 @@ class Database:
                 print("Note: Inadequate data for PlaceVector creation:",
                       instance.name)
 
-        print()
-        search_name = input("Enter the name of a place that you want to compare with others: ")
-        filter_county = input("Would you like to filter by a county? If not, just press Enter: ")
-        print()
+        # Code below to be moved into geodata
 
-        # Obtain the PlaceVector for which we entered a name.
-        comparison_pv = \
-            list(filter(lambda x: x.name == search_name, self.placevectors))[0]
+        # print()
+        # search_name = input("Enter the name of a place that you want to compare with others: ")
+        # filter_county = input("Would you like to filter by a county? If not, just press Enter: ")
+        # print()
 
-        print("The most demographically similar places are:")
-        print()
+        # # Obtain the PlaceVector for which we entered a name.
+        # comparison_pv = \
+        #     list(filter(lambda x: x.name == search_name, self.placevectors))[0]
 
-        # Filter by county if a filter_county was specified.
-        if filter_county != '':
-            filtered_pvs = list(filter(lambda x: x.county == filter_county,
-                                self.placevectors))
+        # print("The most demographically similar places are:")
+        # print()
 
-        # Get the closest PlaceVectors.
-        # In other words, get the most demographically similar places.
-        closest_pvs = sorted(filtered_pvs,
-            key=lambda x: comparison_pv.distance(x))[1:10]
+        # # Filter by county if a filter_county was specified.
+        # if filter_county != '':
+        #     filtered_pvs = list(filter(lambda x: x.county == filter_county,
+        #                         self.placevectors))
 
-        # Print these PlaceVectors
-        for closest_pv in closest_pvs:
-            print(closest_pv)
+        # # Get the closest PlaceVectors.
+        # # In other words, get the most demographically similar places.
+        # closest_pvs = sorted(filtered_pvs,
+        #     key=lambda x: comparison_pv.distance(x))[1:10]
+
+        # # Print these PlaceVectors
+        # for closest_pv in closest_pvs:
+        #     print(closest_pv)
